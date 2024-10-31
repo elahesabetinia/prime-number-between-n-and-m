e0ad0c422be56784727bac610dca1c6b65d78eb0
elahe sabetinia <saikikusou35@gmail.com>
2024-10-31 10:14:36 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //input
+            Console.Write("Enter the starting number (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the ending number (m): ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Prime numbers between {n} and {m}:");
+            for (int i = n; i <= m; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        // Method to check if a number is prime
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false; // Numbers less than 2 are not prime
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
